737ee1bfec3c55b7fd9cbcea1ae725bbf05fe4f8
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-05-06 18:59:56 -0300

    DB linked to compose page only

--- app.js
+++ app.js
@@ -16,17 +16,6 @@ const itemSchema = new mongoose.Schema({
 
 const Post = mongoose.model('Post', itemSchema);
 
-const itemUno = new Post({
-  title: 'Primer post',
-  body: 'Lorem ipsum'
-});
-
-Post.insertMany(itemUno, function(err) {
-  if(err) {
-    console.log(err);
-  }
-});
-
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -38,15 +27,24 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
+// const posts = [];
 
 //Home page rout
 app.get('/', function(req, res) {
 
-  res.render('home', {
-    homeStartingText: homeStartingContent,
-    postsArray: posts
+  Post.find({}, function(err, posts) {
+    if(!err) {
+      res.render('home', {
+        homeStartingText: homeStartingContent,
+        posts: posts
+      });
+    } else {
+      console.log(err);
+    }  
   });
+
+
+  
   
 });
 
@@ -78,12 +76,16 @@ app.get('/compose', function(req, res) {
 
 app.post('/compose', function(req, res) {
 
-  let newPost = {
-    postTitle: req.body.composeInput,
-    postContent: req.body.textareaInput
-  };
+  
+  const postTitle = req.body.composeInput;
+  const postContent = req.body.textareaInput;  
 
-  posts.push(newPost);
+  const newPost = new Post({
+    title: postTitle,
+    body: postContent
+  });
+
+  newPost.save();
 
   res.redirect('/');
 
