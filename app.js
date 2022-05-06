5729bde9dac7b076d7c9ca115cbe36a036f54ac8
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-05-06 08:29:21 -0300

    Database added

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { indexOf } = require("lodash");
-
-//Loading Lodash. Load the full build.
+//Loading Lodash.
 const _ = require('lodash');
+
+//DataBase Initial Config
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/blogDB');
+
+const itemSchema = new mongoose.Schema({
+  title: String,
+  body: String
+});
+
+const Post = mongoose.model('Post', itemSchema);
+
+const itemUno = new Post({
+  title: 'Primer post',
+  body: 'Lorem ipsum'
+});
+
+Post.insertMany(itemUno, function(err) {
+  if(err) {
+    console.log(err);
+  }
+});
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
